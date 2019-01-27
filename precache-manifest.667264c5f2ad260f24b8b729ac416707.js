b05e51576c365e07c4384a9d8e328aa3aa8433fe
Ruslan <ruslan.ishemgulov@gmail.com>
2019-01-27 18:42:21 +0100

    Updates

---
+++ precache-manifest.667264c5f2ad260f24b8b729ac416707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "b6f0457260f5d13dfc32",
+    "url": "/react-app/static/js/main.b6f04572.chunk.js"
+  },
+  {
+    "revision": "bddec3ac5aa69ba8d471",
+    "url": "/react-app/static/js/1.bddec3ac.chunk.js"
+  },
+  {
+    "revision": "b6f0457260f5d13dfc32",
+    "url": "/react-app/static/css/main.14796b0c.chunk.css"
+  },
+  {
+    "revision": "bddec3ac5aa69ba8d471",
+    "url": "/react-app/static/css/1.a4f9172d.chunk.css"
+  },
+  {
+    "revision": "59aceb35b17c14e237cc3795aa78d119",
+    "url": "/react-app/index.html"
+  }
+];
